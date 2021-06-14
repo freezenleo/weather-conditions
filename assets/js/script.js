288673d6d1e83a3674280fa13d4c00fba5bec843
Clark <81452611+freezenleo@users.noreply.github.com>
2021-06-13 23:20:06 -0500

    get current city info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var searchBarEl = document.querySelector("#search-bar");
 var cityNameEl = document.querySelector("#cityname");
 var cityListBtn = document.querySelector("#citylist-btn");
-var currentCityInfoEl = document.querySelector("#current-city-info")
+var currentSearchCityEl = document.querySelector("#current-search-city");
+var currentCityInfoEl = document.querySelector("#current-city-info");
 
 var cityNameSubmit = function (event) {
     event.preventDefault();
@@ -28,21 +29,84 @@ var displayWeather = function (data, searchTerm) {
 
     //check if api returned any data
     if (data.length === 0) {
-        currentCityInfoEl.textContent = "No Weather Info Found";
+        currentSearchCityEl.textContent = "No Weather Info Found";
         return;
     }
 
-    //clear old content
-    currentCityInfoEl.textContent = "";
-    currentCityInfoEl.textContent = searchTerm;
+    var lati = data.coord.lat;
+    var longi = data.coord.lon;
+    var apiUrlLatLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&units=imperial&appid=dc958acf9753f5bb2aae083119c7a2da";
 
-    var
+    fetch(apiUrlLatLon)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayLonLat(data);
+                    console.log("lon&lat", data);
+                })
+            }
+            else {
+                alert("Error: city not found");
+            }
+        })
+
+
+    //clear old content
+    currentSearchCityEl.textContent = "";
+    currentSearchCityEl.textContent = searchTerm;
+
+    var timestamp = new Date(data.dt * 1000);
+    var day = timestamp.getDate();
+    var month = timestamp.getMonth();
+    var year = timestamp.getFullYear();
+    var date = month + "/" + day + "/" + year;
+    console.log("dateString", date);
+
+
+    var dateInput = document.createElement("span");
+    dateInput.addClass = "date";
+    dateInput.textContent = "   " + date + "   ";
+    currentSearchCityEl.appendChild(dateInput);
+
+    var imgEl = document.createElement("img");
+    imgEl.addClass = "wicon";
+    imgEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+    currentSearchCityEl.appendChild(imgEl);
 }
 
+var displayLonLat = function (data) {
+    var tempEl = document.createElement("li");
+    tempEl.classList = "list-group-item";
+    tempEl.textContent = "Temp: " + data.current.temp + " \u00B0F";
+    currentCityInfoEl.appendChild(tempEl);
+
+    var windEl = document.createElement("li");
+    windEl.classList = "list-group-item";
+    windEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    currentCityInfoEl.appendChild(windEl);
+
+    var humidityEl = document.createElement("li");
+    humidityEl.classList = "list-group-item";
+    humidityEl.textContent = "Humidity: " + data.current.humidity + " % ";
+    currentCityInfoEl.appendChild(humidityEl);
+
+    var uvIndexEl = document.createElement("li");
+    uvIndexEl.classList = "list-group-item";
+    uvIndexEl.textContent = "UV Index: " + data.current.uvi;
+    currentCityInfoEl.appendChild(uvIndexEl);
+}
+
+var displayForecast = function (data) {
+    for (var i = 0; i < daily.length; i++) {
+
+    }
+
+}
 
 //fetch data from sever
 var getSearchCity = function (cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=dc958acf9753f5bb2aae083119c7a2da";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=dc958acf9753f5bb2aae083119c7a2da";
+
     //make a request to the url
     fetch(apiUrl)
         .then(function (response) {
